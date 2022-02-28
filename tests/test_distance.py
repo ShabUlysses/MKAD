1c96da014e06140949bf8fd99884ac6ab4af560b
ShabUlysses <shab.ulysses@gmail.com>
2022-02-28 20:22:52 +0300

    Added new tests

--- tests/test_distance.py
+++ tests/test_distance.py
@@ -1,14 +1,12 @@
 import unittest
 from decimal import Decimal
 
+from shapely.geometry import Point
+
 from configs.config import api_key
 from geocoder_api import Client
-from main.distance_calculator import find_distance_to_polygon
+from main.distance_calculator import find_distance_to_polygon, mkad
 
-
-# client = Client(api_key)
-# lat, lon = Decimal(54.719362), Decimal(20.505681)
-# print(find_distance_to_polygon((lat, lon)))
 
 class Test(unittest.TestCase):
     def test_find_distance_with_address(self):
@@ -20,11 +18,13 @@ class Test(unittest.TestCase):
         lat, lon = Decimal(54.719362), Decimal(20.505681)
         self.assertEqual(find_distance_to_polygon((lat, lon)), 1072)
 
-    def address_inside_mkad(self):
+    def test_address_inside_mkad(self):
         client = Client(api_key)
         coordinates = client.transform_to_coordinates('Красная площадь')
-        self.assertEqual(find_distance_to_polygon(coordinates), 'Точка находится внутри МКАДа!')
+        self.assertEqual(mkad.contains(Point(coordinates)), True) # True if point is inside MKAD
 
-    def coordinates_inside_mkad(self):
-        lat, lon = Decimal(55.781205), Decimal(37.599525)
-        self.assertEqual(find_distance_to_polygon((lat, lon)), 'Точка находится внутри МКАДа!')
+    def test_coordinates_inside_mkad(self):
+        lat, lon = Decimal(55.778892), Decimal(37.59542)
+        self.assertEqual(mkad.contains(Point((lat, lon))), True) # True if point is inside MKAD
+
+
