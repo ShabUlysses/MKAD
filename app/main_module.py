f6d10fc4a96326bd2b8f4ba5c3101b0c01571752
ShabUlysses <shab.ulysses@gmail.com>
2022-03-02 15:25:24 +0300

    Commented the code

--- app/main_module.py
+++ app/main_module.py
@@ -8,7 +8,7 @@ from config import api_key
 from .client import Client
 from .distance_calculator import mkad, find_distance_to_polygon
 
-main = Blueprint('app', __name__)
+main = Blueprint('app', __name__)  # Create the blueprint of application
 
 log = logging.getLogger('werkzeug')
 log.disabled = True  # Setting the logging to save only results of http query
@@ -22,27 +22,31 @@ def welcome_page():
 
 @main.route('/address', methods=['GET'])
 def enter_address():
-    address = request.args.get('address')
-    client = Client(api_key)
-    coordinates = client.transform_to_coordinates(address)
-    if mkad.contains(Point(coordinates)):
+    """This function accepts http-query in form of address from user and returns the distance to MKAD.
+    If given address is inside MKAD, then it return associated message."""
+    address = request.args.get('address')  # Get address
+    client = Client(api_key)  # Connect to yandex-geocoder
+    coordinates = client.transform_to_coordinates(address)  # Transform address to coordinates
+    if mkad.contains(Point(coordinates)): # Check if coordinates are inside MKAD
         current_app.logger.info(f'{address} находится внутри МКАДа')
         return '''Точка находится внутри МКАДа!'''
     else:
-        distance = find_distance_to_polygon(coordinates, mkad)
+        distance = find_distance_to_polygon(coordinates, mkad)  # Calculate the distance
         current_app.logger.info(f'Расстояние от точки "{address}" до МКАДа составляет {distance} км')
         return f'Расстояние от точки: "{address}" до МКАДа составляет {distance} км'
 
 
 @main.route('/coords', methods=['GET'])
 def enter_coordinates():
-    lat = request.args.get('lat')
-    lon = request.args.get('lon')
-    coordinates = Decimal(lat), Decimal(lon)
-    if mkad.contains(Point(coordinates)):
+    """This function accepts http-query in form of coordinates from user and returns the distance to MKAD.
+    If given coordinates are inside MKAD, then it return associated message."""
+    lat = request.args.get('lat')  # Get latitude
+    lon = request.args.get('lon')  # Get longitude
+    coordinates = Decimal(lat), Decimal(lon)  # Transform coordinates to a tuple of Decimals.
+    if mkad.contains(Point(coordinates)):  # Check if coordinates are inside MKAD
         current_app.logger.info(f'Точка {lat, lon} находится внутри МКАДа')
         return '''Точка находится внутри МКАДа!'''
     else:
-        distance = find_distance_to_polygon(coordinates, mkad)
+        distance = find_distance_to_polygon(coordinates, mkad)  # Calculate the distance
         current_app.logger.info(f'Расстояние от точек: {lat, lon} до МКАДа составляет {distance} км')
         return f'Расстояние от точек {lat, lon} до МКАДа составляет {distance} км'
